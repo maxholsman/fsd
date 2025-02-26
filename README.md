ec3ce8dcd67ae37ed67de53e3e1ffcd867687b42
maxholsman <maxholsman@gmail.com>
2025-02-26 08:17:17 -0500

    updaed FSDAutoModelForCausalLM

--- README.md
+++ README.md
@@ -7,44 +7,21 @@ This is the official fuzzy speculative decoding implementation used to run exper
 We designed our implementation on top of the huggingface `transformers` library for easy use. As such, our implementation modifies the default `model.generate` assisted decoding functionality to also allow for fuzzy speculative decoding in addition to regular speculative decoding. 
 
 To use our implementation, follow the steps below: 
-1. Install `transformers` version 4.44
-2. Install `fsd_utils.py`, which is a modified version of `transformers/generation/utils.py`, with your method of choice.
-3. Import the necessary `transformers` classes, as well as `FuzzyGenerationMixin` from `fsd_utils.py`
+1. Install `transformers`==4.44
+2. Our modified `model.generate` functionality can easily be accessed via custom `ForCausalLM` classes. Currently, we have implemented custom classes for all models tested in our paper, namely `LlamaForCausalLM`, `Gemma2ForCausalLM` and `Qwen2ForCausalLM`. These our custom version of these three model classes can be accessed through a custom class `FSDAutoModelForCausalLM`. Thus, to initialize:
 
 ```python
-from transformers import AutoModelForCausalLM, AutoTokenizer
-from fsd.fsd_utils import FuzzyGenerationMixin
-```
+from transformers
+from fsd.fsd_utils import FSDAutoModelForCausalLM
 
-4. When defining the target and draft models, reroute the use of the default `GenerationMixin` with:
+small_model_id = 'google/gemma-2-2b-it'
+large_model_id = 'google/gemma-2-27b-it'
 
-```python
 small_tokenizer = AutoTokenizer.from_pretrained(small_model_id)
-small_model = AutoModelForCausalLM.from_pretrained(small_model_id, torch_dtype=torch.bfloat16).to(device0)
-large_model = AutoModelForCausalLM.from_pretrained(large_model_id, torch_dtype=torch.bfloat16, device_map='auto')
-
-def patch_generation(model):
-    """Patches model.generate functions to use modified code from FuzzyGenerationMixin"""
-    
-    # Preserve model-specific `prepare_inputs_for_generation`
-    if not hasattr(model, "prepare_inputs_for_generation") or model.prepare_inputs_for_generation.__func__ == fsd_utils.FuzzyGenerationMixin.prepare_inputs_for_generation:
-        # Get method from original model class (e.g., GemmaForCausalLM)
-        for base_class in model.__class__.__mro__:
-            if "prepare_inputs_for_generation" in base_class.__dict__:
-                model.prepare_inputs_for_generation = base_class.prepare_inputs_for_generation.__get__(model, model.__class__)
-                break  # Stop once we find it
-    
-    # Override generation-related methods with fsd_utils
-    model.generate = fsd_utils.FuzzyGenerationMixin.generate.__get__(model, model.__class__)
-    model._backoff_assisted_decoding = fsd_utils.FuzzyGenerationMixin._backoff_assisted_decoding.__get__(model, model.__class__)
-    model._get_candidate_generator = fsd_utils.FuzzyGenerationMixin._get_candidate_generator.__get__(model, model.__class__)
-    model._update_model_kwargs_for_generation = fsd_utils.FuzzyGenerationMixin._update_model_kwargs_for_generation.__get__(model, model.__class__)
-
-
-# Apply the patch to both models
-patch_generation(small_model)
-patch_generation(large_model)
+small_model = FSDAutoModelForCausalLM.from_pretrained(small_model_id, torch_dtype=torch.bfloat16).to(device0)
+large_model = FSDAutoModelForCausalLM.from_pretrained(large_model_id, torch_dtype=torch.bfloat16, device_map='auto')
 ```
+
 5. Use FSD as you would use regular speculative decoding by passing as assistant model to the target model's `model.generate` call. Set the divergence type with the `fsd_div_type` parameter (defaults to JS divergence), and the div threshold with the `fsd_div_threshold` parameter. *Whether FSD or traditional SD is run depends on whether `fsd_div_threshold` is set to a value - if this parameter is not passed into `model.generate`, regular SD will run*
 
 ```python 
@@ -56,6 +33,7 @@ output = large_model.generate(**input_ids, assistant_model=small_model, fsd_div_
 print(f"output: {output}")
 print(f"output: {small_tokenizer.decode(output[0])}")
 ```
+
 The divergence options are KL divergence (`kl_div`), JS divergence (`js_div`), TV distance (`tv_div`), as well as top-K and top-P variants of these three divergences (`top_k_kl_div`, `top_k_js_div`, `top_k_tv_div`). Note that the `fsd_div_threshold` is highly dependent on which divergence type is being used. 
 
 
--- fsd_utils.py
+++ fsd_utils.py
@@ -10,6 +10,7 @@ import torch
 import torch.distributed as dist
 from torch import nn
 from torch.nn import functional as F
+from transformers import AutoModelForCausalLM, LlamaForCausalLM, Gemma2ForCausalLM, Qwen2ForCausalLM, AutoConfig
 
 from transformers.cache_utils import (
     Cache,
@@ -127,6 +128,204 @@ from transformers.generation.candidate_generator import CandidateGenerator
 if TYPE_CHECKING:
     from transformers.modeling_utils import PreTrainedModel
     from transformers.generation.configuration_utils import GenerationConfig
+    
+class AssistedCandidateGenerator(CandidateGenerator): # VERY slightly changed AssistedCandidateGenerator class to return both candidates AND logits (as opposed to just candidates)
+    """
+    `CandidateGenerator` class to be used for assisted generation and speculative decoding. This class generates
+    candidates through the use of a smaller model. Read the following blog post for more information:
+    https://huggingface.co/blog/assisted-generation
+
+    Args:
+        input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
+            Indices of input sequence tokens in the vocabulary. [What are input IDs?](../glossary#input-ids)
+        assistant_model (`PreTrainedModel`):
+            The model to be used for generating candidates. This model should be smaller than the main model.
+        generation_config (`~generation.GenerationConfig`, *optional*):
+            The generation configuration to be used as base parametrization for the generation call.
+        logits_processor (`LogitsProcessorList`):
+            An instance of [`LogitsProcessorList`]. List of instances of class derived from [`LogitsProcessor`]
+            used to modify the prediction scores of the language modeling head applied at each generation step.
+        model_kwargs (`Dict`):
+            The keyword arguments that will be passed to the main model, and are used as base inputs for the assistant
+            model as well.
+        inputs_tensor (`torch.Tensor`, *optional*):
+            The model input tensor. In encoder-decoder models, this is the encoder input.
+    """
+    
+    def __init__(
+        self,
+        input_ids: torch.LongTensor,
+        assistant_model: "PreTrainedModel",
+        generation_config: "GenerationConfig",
+        model_kwargs: Dict,
+        inputs_tensor: Optional[torch.Tensor] = None,
+        logits_processor: "LogitsProcessorList" = None,
+    ):
+        # Make sure all data at the same device as assistant model
+        device = assistant_model.device
+        input_ids = input_ids.to(device)
+        if inputs_tensor is not None:
+            inputs_tensor = inputs_tensor.to(device)
+        
+        # Prepare the assistant and the starting number of candidate tokens
+        self.assistant_model = assistant_model
+        self.num_assistant_tokens = assistant_model.generation_config.num_assistant_tokens
+        self.num_assistant_tokens = 5
+        
+        # Set eos in assistant same as in target model
+        self.assistant_model.generation_config.eos_token_id = generation_config.eos_token_id
+        self.assistant_model.generation_config.num_assistant_tokens_schedule = 'None'
+        
+        # Prepare the kwargs for the assistant model
+        assistant_kwargs = {}
+        for key, value in model_kwargs.items():  # deepcopy crashes if we attempt to copy encoder outputs with grads
+            if key not in ("encoder_outputs", "assistant_encoder_outputs", "past_key_values"):
+                assistant_kwargs[key] = (
+                    value.detach().to(device) if isinstance(value, torch.Tensor) else copy.deepcopy(value)
+                )
+
+        if "assistant_encoder_outputs" in model_kwargs:
+            assistant_kwargs["encoder_outputs"] = model_kwargs["assistant_encoder_outputs"]
+        elif assistant_model.config.is_encoder_decoder:
+            inputs_tensor, model_input_name, assistant_kwargs = assistant_model._prepare_model_inputs(
+                inputs_tensor, assistant_model.generation_config.bos_token_id, assistant_kwargs
+            )
+            assistant_kwargs = assistant_model._prepare_encoder_decoder_kwargs_for_generation(
+                inputs_tensor, assistant_kwargs, model_input_name, assistant_model.generation_config
+            )
+        elif "encoder_outputs" in model_kwargs:
+            assistant_kwargs["encoder_outputs"] = model_kwargs["encoder_outputs"]
+        self.assistant_kwargs = assistant_kwargs
+
+        # Prepare assistant model's keys of inputs
+        if assistant_model.config.is_encoder_decoder:
+            # both are encoder-decoder
+            self.input_ids_key = "decoder_input_ids"
+        elif "encoder_outputs" in assistant_kwargs:
+            # special case for encoder-decoder with decoder-only assistant (like DistilWhisper)
+            self.input_ids_key = "input_ids"
+            self.assistant_kwargs["attention_mask"] = self.assistant_kwargs.get(
+                "decoder_attention_mask",
+                torch.ones((input_ids.shape[0], 1), device=input_ids.device, dtype=torch.long),
+            )
+        else:
+            # both are decoder-only
+            self.input_ids_key = "input_ids"
+
+        # Prepare generation-related options.
+        self.logits_processor = logits_processor if logits_processor is not None else LogitsProcessorList()
+        self.generation_config = copy.deepcopy(generation_config)
+        self.generation_config.return_dict_in_generate = True
+        self.generation_config.output_scores = True
+        self.generation_config.output_logits = True
+
+        # Disable sampling -- this implementation of assisted generation/speculative decoding uses the assistant
+        # greedily to maximize matches. Disables sampling-related flags to prevent warnings
+        
+        # print(f"self.generation_config.do_sample: {self.generation_config.do_sample}")
+        self.generation_config.do_sample = False # Change this back to False
+        for attr in ("temperature", "top_p", "min_p", "typical_p", "top_k", "epsilon_cutoff", "eta_cutoff"):
+            setattr(self.generation_config, attr, None)
+        
+        # avoid unnecessary warnings that min_length is larger than max_new_tokens
+        # remove the `MinLengthLogitsProcessor` if exists (NOTE: no need to check for `MinNewTokensLogitsProcessor`)
+        self.main_model_min_length = self.generation_config.min_length
+        self.generation_config.min_length = 0
+        self.generation_config.min_new_tokens = None
+        for processor in self.logits_processor:
+            if isinstance(processor, MinLengthLogitsProcessor):
+                raise ValueError(
+                    "Passing `MinLengthLogitsProcessor` when using `assisted_generation is disabled. "
+                    "Please pass in `min_length` into `.generate()` instead"
+                )
+
+        # We need to roll back the cache in assisted generation, only DynamicCache is supported
+        self.generation_config.cache_implementation = None
+
+    def get_candidates(self, input_ids: torch.LongTensor) -> Tuple[torch.LongTensor, Optional[torch.FloatTensor]]:
+        """
+        Fetches the candidates to be tried for the current input.
+
+        `A`rgs:
+            input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
+                Indices of input sequence tokens in the vocabulary. [What are input IDs?](../glossary#input-ids)
+
+        Return:
+            `torch.LongTensor` of shape `(batch_size, candidate_length)` containing the candidate sequences to be
+            assessed by the model and a `torch.FloatTensor` of shape `(batch_size, candidate_length,
+            vocabulary_size)` containing the logits associated to each candidate.
+        
+        """
+        
+        input_ids = input_ids.to(self.assistant_model.device)
+
+        # Don't generate more than `max_length - 1` candidates since the target model generates one extra token.
+        new_cur_len = input_ids.shape[-1]
+        max_new_tokens = min(int(self.num_assistant_tokens), self.generation_config.max_length - new_cur_len - 1)
+        min_new_tokens = max(min(max_new_tokens, self.main_model_min_length - new_cur_len), 0)
+        # print(f"max_new_tokens: {max_new_tokens} - num assistant tokens: {self.num_assistant_tokens} - max_length: {self.generation_config.max_length} - new cur len: {new_cur_len} - main_model: {self.main_model_min_length}")
+        if max_new_tokens == 0:
+            return input_ids, None, None
+
+        # 1. If it is not the first round of candidate generation, prepare the inputs based on the input_ids length 
+        # (which implicitly contains the number of accepted candidates from the previous round)
+        has_past_key_values = self.assistant_kwargs.get("past_key_values", None) is not None
+        if has_past_key_values:
+            new_cache_size = new_cur_len - 1
+            self.assistant_kwargs["past_key_values"] = _crop_past_key_values(
+                self.assistant_model, self.assistant_kwargs["past_key_values"], new_cache_size - 1
+            )  # the assistant does not have the token after the last match, hence the -1
+
+            self.assistant_kwargs = _prepare_attention_mask(
+                self.assistant_kwargs, new_cur_len, self.assistant_model.config.is_encoder_decoder
+            )
+            self.assistant_kwargs = _prepare_token_type_ids(self.assistant_kwargs, new_cur_len)
+
+        # 2. Forecast next N tokens using the assistant model.
+        assistant_generation_kwargs = {
+            self.input_ids_key: input_ids,
+            "min_new_tokens": min_new_tokens,
+            "max_new_tokens": max_new_tokens,
+            "generation_config": self.generation_config,
+            "logits_processor": self.logits_processor,
+        }
+        
+        assistant_output = self.assistant_model.generate(**assistant_generation_kwargs, **self.assistant_kwargs)
+
+        # 3. Update variables for the next round of candidate generation
+        self.assistant_kwargs["past_key_values"] = assistant_output.past_key_values
+
+        # 4. Prepare variables for output
+        candidate_logits = torch.stack(assistant_output.scores, dim=1)
+        candidate_logits_unprocessed = torch.stack(assistant_output.logits, dim=1)
+        candidate_ids = assistant_output.sequences
+        return candidate_ids, candidate_logits, candidate_logits_unprocessed
+
+    def update_candidate_strategy(self, input_ids: torch.LongTensor, scores: torch.FloatTensor, num_matches: int):
+        """
+        Updates the candidate generation strategy based on the outcomes.
+
+        Args:
+            input_ids (`torch.LongTensor` of shape `(batch_size, sequence_length)`):
+                Indices of input sequence tokens in the vocabulary. [What are input IDs?](../glossary#input-ids)
+            scores (`torch.FloatTensor` of shape `(batch_size, candidate_length, config.vocab_size)`):
+                Prediction scores of a language modeling head. These can be logits for each vocabulary when not using
+                beam search or log softmax for each vocabulary token when using beam search
+            num_matches (`int`):
+                The number of matches between the candidate sequences and the model predictions.
+        """
+        # Adjust the max number of assistant tokens to use in the next iteration. This is a simple heuristic,
+        # probably can be improved -- we want to balance the benefits of getting assistant tokens correct with the
+        # cost of forecasting incorrect assistant tokens.
+        if self.assistant_model.generation_config.num_assistant_tokens_schedule in {
+            "heuristic",
+            "heuristic_transient",
+        }:
+            if num_matches == int(self.num_assistant_tokens):
+                self.num_assistant_tokens += 2.0
+            else:
+                self.num_assistant_tokens = max(1.0, self.num_assistant_tokens - 1.0)
+
     
 class FuzzyAssistedCandidateGenerator(CandidateGenerator):
     """
@@ -1450,6 +1649,10 @@ class FuzzyGenerationMixin:
 
         return transition_scores
 
+    @classmethod
+    def can_generate(cls) -> bool:
+        return True  # Force models using this mixin to be considered valid for generation
+    
     def _validate_model_class(self):
         """
         Confirms that the model class is compatible with generation. If not, raises an exception that points to the
@@ -6322,3 +6525,48 @@ def _dola_select_contrast(
     final_logits, base_logits = _relative_top_filter(final_logits, base_logits)
     logits = final_logits - base_logits
     return logits
+
+
+
+class FSDLlamaForCausalLM(FuzzyGenerationMixin, LlamaForCausalLM):
+    def prepare_inputs_for_generation(self, input_ids, **kwargs):
+        return LlamaForCausalLM.prepare_inputs_for_generation(self, input_ids, **kwargs)
+
+class FSDGemma2ForCausalLM(FuzzyGenerationMixin, Gemma2ForCausalLM):
+    def prepare_inputs_for_generation(self, input_ids, **kwargs):
+        return Gemma2ForCausalLM.prepare_inputs_for_generation(self, input_ids, **kwargs)
+
+class FSDQwen2ForCausalLM(FuzzyGenerationMixin, Qwen2ForCausalLM):
+    def prepare_inputs_for_generation(self, input_ids, **kwargs):
+        return Qwen2ForCausalLM.prepare_inputs_for_generation(self, input_ids, **kwargs)
+
+# class FSDAutoModelForCausalLM(AutoModelForCausalLM, FuzzyGenerationMixin):
+#     # Ensure correct method resolution order (MRO)
+#     pass
+
+MODEL_MAPPING = {
+    "LlamaForCausalLM": FSDLlamaForCausalLM,
+    "Gemma2ForCausalLM": FSDGemma2ForCausalLM,
+    "Qwen2ForCausalLM": FSDQwen2ForCausalLM,
+}
+
+class FSDAutoModelForCausalLM:
+    @classmethod
+    def from_pretrained(cls, model_name_or_path, *args, **kwargs):
+        # Load config
+        config = AutoConfig.from_pretrained(model_name_or_path)
+
+        # Ensure architectures field exists
+        model_class_name = getattr(config, "architectures", [None])[0]
+        if model_class_name is None:
+            raise ValueError(f"Could not determine model class from config: {model_name_or_path}")
+
+        # Find the custom FSD model
+        fsd_model_class = MODEL_MAPPING.get(model_class_name, None)
+        if fsd_model_class is None:
+            raise ValueError(f"Unsupported model class {model_class_name}")
+
+        print(f"Loading model: {fsd_model_class.__name__}")  # Debugging
+
+        # Load and return the model
+        return fsd_model_class.from_pretrained(model_name_or_path, *args, **kwargs)
